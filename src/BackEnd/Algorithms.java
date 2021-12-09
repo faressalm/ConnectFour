aa0240fdfa006088aa10cc5bdb38bbe2023d7e8b
fares waheed <faressalm44@hotmail.com>
2021-12-09 18:55:47 +0200

    update

--- src/BackEnd/Algorithms.java
+++ src/BackEnd/Algorithms.java
@@ -3,6 +3,7 @@ package BackEnd;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 
 public class Algorithms {
@@ -14,6 +15,7 @@ public class Algorithms {
     private final int maxDepth;
     private boolean pruning;
     private String[] tree;
+    private HashMap<Long, HashMap<Long, Integer>> dp;
 
     public Algorithms(int maxDepth, boolean pruning) {
         this.maxDepth = maxDepth;
@@ -26,9 +28,13 @@ public class Algorithms {
     }
 
     public int minimax() {
+        Long s, e;
+        s = System.currentTimeMillis();
         int col;
         tree = new String[maxDepth + 1];
         Arrays.fill(tree, "");
+        // initialize the dp
+        //dp = new HashMap<Long , HashMap<Long, Integer> >();
         if (pruning) {
             Integer alpha = Integer.MIN_VALUE;
             Integer beta = Integer.MAX_VALUE;
@@ -36,6 +42,10 @@ public class Algorithms {
         } else
             col = minimax(this.human, this.agent, 0, true)[1];
         updateAgent(col);
+        e = System.currentTimeMillis();
+        if (pruning) System.out.println("Time with Pruning : " + (e - s) + " ms");
+        else System.out.println("Time without Pruning : " + (e - s) + " ms");
+        System.out.println("------------------------------------------");
         return 6 - col;
     }
 
@@ -49,8 +59,60 @@ public class Algorithms {
         return cols;
     }
 
-    private int count(Long player, int piles) {
-        int cnt = 0;
+    private int[] countDef(Long player, Long otherPlayer) {
+        int[] cnts = new int[8];
+        Arrays.fill(cnts, 8);
+        Boolean[][] arr = manipulation.get2D(otherPlayer);
+        Boolean[][] me = manipulation.get2D(player);
+        int adj = 0;
+        int[] diagP = new int[12], diagN = new int[12];
+        Arrays.fill(diagN, 0);
+        Arrays.fill(diagP, 0);
+        /* Get Rows counts  && set Diagonals counts */
+        for (int i = 0; i < 6; i++) {
+            adj = 0;
+            for (int j = 0; j < 7; j++) {
+                if (arr[i][j]) {
+                    diagN[i + j]++;
+                    diagP[5 + (j - i)]++;
+                    adj++;
+                    if (j == 6) {
+                        if (adj > 1 && adj < 7 && me[i][j - adj]) cnts[adj]++;
+                        if (diagN[i + j] > 1 && diagN[i + j] < 7 && me[i][j - diagN[i + j]]) cnts[diagN[i + j]]++;
+                        if (diagP[5 + (j - i)] > 1 && diagP[5 + (j - i)] < 7 && me[i][j - diagP[5 + (j - i)]])
+                            cnts[diagP[5 + (j - i)]]++;
+                    }
+                } else {
+                    if (adj > 1 && me[i][j]) cnts[adj]++;
+                    if (diagN[i + j] > 1 && me[i][j]) cnts[diagN[i + j]]++;
+                    if (diagP[5 + (j - i)] > 1 && me[i][j]) cnts[diagP[5 + (j - i)]]++;
+                    diagN[i + j] = 0;
+                    diagP[5 + (j - i)] = 0;
+                    adj = 0;
+                }
+            }
+        }
+        /*  Get Cols */
+        for (int i = 0; i < 7; i++) {
+            adj = 0;
+            for (int j = 0; j < 6; j++) {
+                if (!arr[j][i]) {
+                    if (adj > 1 && me[j][i]) cnts[adj]++;
+                    adj = 0;
+                } else {
+                    adj++;
+                    if (j == 5) {
+                        if (adj > 1 && adj < 6 && me[j - adj][i]) cnts[adj]++;
+                    }
+                }
+            }
+        }
+        return cnts;
+    }
+
+    private int[] count(Long player) {
+        int[] cnts = new int[8];
+        Arrays.fill(cnts, 8);
         Boolean[][] arr = manipulation.get2D(player);
         int adj = 0;
         int[] diagP = new int[12], diagN = new int[12];
@@ -64,14 +126,13 @@ public class Algorithms {
                     diagN[i + j]++;
                     diagP[5 + (j - i)]++;
                     adj++;
-                    if (diagN[i + j] == piles) cnt++;
-                    if (diagP[5 + (j - i)] == piles) cnt++;
-                    if (adj == piles) cnt++;
+                    if (diagN[i + j] > 1) cnts[diagN[i + j]]++;
+                    if (diagP[5 + (j - i)] > 1) cnts[diagP[5 + (j - i)]]++;
+                    if (adj > 1) cnts[adj]++;
                 } else {
                     diagN[i + j] = 0;
                     diagP[5 + (j - i)] = 0;
                     adj = 0;
-
                 }
             }
         }
@@ -82,11 +143,11 @@ public class Algorithms {
                 if (!arr[j][i]) adj = 0;
                 else {
                     adj++;
-                    if (adj == piles) cnt++;
+                    if (adj > 1) cnts[adj]++;
                 }
             }
         }
-        return cnt;
+        return cnts;
     }
 
     private int destroy(Long player) {
@@ -97,12 +158,20 @@ public class Algorithms {
     private int getHeuristic(Long human, Long agent) {
         int agentFeatures = 0, humanFeatures = 0;
         int value = 10;
-        int humanCnt, agentCnt;
+        int[] humanCnt, agentCnt;
+        humanCnt = count(human);
+        agentCnt = count(agent);
         for (int piles = 2; piles < 8; piles++) {
-            humanCnt = count(human, piles);
-            agentCnt = count(agent, piles);
-            humanFeatures += value * humanCnt;
-            agentFeatures += value * agentCnt;
+            humanFeatures += value * humanCnt[piles];
+            agentFeatures += value * agentCnt[piles];
+            value *= 10;
+        }
+        value = 10;
+        humanCnt = countDef(human, agent);
+        agentCnt = countDef(agent, human);
+        for (int piles = 2; piles < 7; piles++) {
+            humanFeatures += value * humanCnt[piles];
+            agentFeatures += value * agentCnt[piles];
             value *= 10;
         }
         agentFeatures += destroy(agent) * 1000;
@@ -111,6 +180,9 @@ public class Algorithms {
     }
 
     private int[] minimax(Long human, Long agent, int depth, boolean maximizingPlayer) {
+        /*if(dp.containsKey(human) && dp.get(human).containsKey(agent) ){
+            return  dp.get(human).get(agent)  ;
+        }*/
         ArrayList<Integer> cols = getNext(agent | human);
         int features, col = 0;
         Long board = human | agent;
@@ -192,7 +264,6 @@ public class Algorithms {
         if (!manipulation.isValid(col, human | agent)) {
             System.out.println("Not Valid");
         }
-        //System.out.println(col);
         int row = manipulation.getRowOrder(human | agent, col);
         this.human = human | (1L << (row * 7 + col));
         this.humanScore = manipulation.getScore(human);
@@ -238,4 +309,5 @@ public class Algorithms {
         if ((human | agent) == 4398046511103L) return true;
         return false;
     }
+
 }
